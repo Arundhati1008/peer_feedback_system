f8b0cdd6e0a79ead4d3402c394430afcf232a56d
Arundhati1008 <thakurarundhati08@gmail.com>
2025-04-29 20:15:40 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,27 @@
-from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
-from feedback.views import project_list
-from django.conf import settings
-from django.conf.urls.static import static
+"""
+URL configuration for peer_feedback_system project.
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('submit/', views.submit_project, name='submit_project'),
-    path('', views.home, name='home'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('register/', views.register, name='register'),
-    path('project/<int:project_id>/', views.project_detail, name='project_detail'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('projects/',project_list, name='project_list'),
+    path('admin/', admin.site.urls),
+    path('', include('feedback.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
